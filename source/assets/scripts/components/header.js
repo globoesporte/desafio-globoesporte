59db8c79bd1a2eeb5f435a2b5173fb06e080f29f
rodmoioliveira <rodmoi.oliveira@gmail.com>
2018-01-18 07:57:15 -0200

    implementa mockup inicial e uuidv para geração de hashs aleatórias para keys dos componentes

--- source/assets/scripts/components/header.js
+++ source/assets/scripts/components/header.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react';
 class TopBar extends Component {
   render() {
     return(
-      <nav className="nav x">
-        <h2 className="nav__title x no-margin">ge</h2>
-      </nav>
+      <header className="nav x">
+        <h1 className="nav__title x no-margin">ge</h1>
+      </header>
     );
   }
 }
--- source/assets/scripts/main.js
+++ source/assets/scripts/main.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
 import ReactDOM  from "react-dom";
-import Topbar from './components/topabar';
-
-// import data from '../../../api/data.json';
-// console.log(data)
+import Header from './components/header';
+import data from '../../../api/data.json';
+import uuidv4 from 'uuid/v4';
 
 class App extends Component {
   render() {
+
+    const todos = data.slice();
+
+    const listaDeItens = todos.map(todo => 
+      <li className="todos__list__item x" key={uuidv4()}>
+        <input className="todos__list__item__checkbox x" type="checkbox" name={uuidv4()} /> 
+        <label className="todos__list__item__text x" htmlFor={uuidv4()}>{todo.text}</label> 
+        <button className="todos__list__item__delete x">x</button>
+      </li>
+    );
+
     return(
-      <Topbar />
+      <main>
+        <Header key={uuidv4()} />
+        <div className="wrapper x">
+          <input className="new-todo-input x" type="text" placeholder="Escreva aqui uma nova tarefa..." />
+          <section className="todos x">
+            <ul className="todos__list x">
+              {listaDeItens}
+            </ul>
+          </section>
+        </div>
+      </main>
     );
   }
 } 
