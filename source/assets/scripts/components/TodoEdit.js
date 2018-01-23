0afce0164eead0c944f166b8ec864c7036a8dbb4
rodmoioliveira <rodmoi.oliveira@gmail.com>
2018-01-23 10:55:36 -0200

    Acrescenta tag form no componente TodoEdit

--- source/assets/scripts/components/TodoEdit.js
+++ source/assets/scripts/components/TodoEdit.js
@@ -3,27 +3,31 @@ import React from 'react';
 export const TodoEdit = (props) => {
   const btnOn = props.editText === '' ? true : false;
   const disableClasse = btnOn ? '__disable' : '';
-  const btnClasses = `todos__list__item__edit__confirm${disableClasse} x `;
+  const btnClasses = `todos__list__item__edit__form__confirm${disableClasse} x `;
   return (
     <li className="todos__list__item__edit x">
-      <input 
-        className="todos__list__item__edit__input x" 
-        type="text" 
-        placeholder={props.text} 
-        value={props.editText}
-        onChange={props.editInputChange}
-        /> 
-      <button 
-        className={btnClasses}
-        onClick={() => props.confirmButtonSubmit(props.id)}
-        disabled={btnOn}>
-          Confirmar
-      </button>
-      <button 
-        className="todos__list__item__edit__cancel x"
-        onClick={() => props.toogleEditTodo(props.id)}>
-          Cancelar
-      </button>
+      <form 
+        className="todos__list__item__edit__form x"
+        onSubmit={() => props.confirmButtonSubmit(props.id)}>
+        <input 
+          className="todos__list__item__edit__form__input x" 
+          type="text" 
+          placeholder={props.text} 
+          value={props.editText}
+          onChange={props.editInputChange}
+          /> 
+        <button 
+          className={btnClasses}
+          onClick={() => props.confirmButtonSubmit(props.id)}
+          disabled={btnOn}>
+            Confirmar
+        </button>
+        <button 
+          className="todos__list__item__edit__form__cancel x"
+          onClick={() => props.toogleEditTodo(props.id)}>
+            Cancelar
+        </button>
+      </form>
     </li>
   );
 
