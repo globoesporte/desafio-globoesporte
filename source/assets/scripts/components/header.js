21429dc190cd1ba318cc3d1c1eb2c3d0530b2b4a
rodmoioliveira <rodmoi.oliveira@gmail.com>
2018-01-23 19:48:26 -0200

    estilização do header

--- source/assets/scripts/components/header.js
+++ source/assets/scripts/components/header.js
@@ -3,5 +3,12 @@ import React from 'react';
 export const Header = () => (
   <header className="header x">
     <h2 className="header__title x no-margin">GE</h2>
+    <section className="header__time x">
+      <h2 className="header__time__day no-margin x">12</h2>
+      <div className="header__time__more-info x">
+        <p className="header__time__more-info__day-week no-margin x">Terça</p>
+        <small className="header__time__more-info__month-year x">JAN 2017</small>
+      </div>
+    </section>
   </header>
 );
