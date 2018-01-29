b16bdabcc73bc00e4cf17faa1966070338f71070
rodmoioliveira <rodmoi.oliveira@gmail.com>
2018-01-29 21:50:06 -0200

    acrescenta imagem para ausências de todos

--- source/assets/scripts/components/TodoEdit.js
+++ source/assets/scripts/components/TodoEdit.js
@@ -7,11 +7,6 @@ export class TodoEdit extends Component {
   }
 
   render() {
-
-    const btnOn = this.props.editText === '' ? true : false;
-    const disableClasse = btnOn ? '__disable' : '';
-    const btnClasses = `todos__list__item__edit__form__confirm${disableClasse} x `;
-
     return (
       <li className="todos__list__item__edit x" >
         <form 
@@ -26,17 +21,6 @@ export class TodoEdit extends Component {
             value={this.props.editText}
             onChange={this.props.editInputChange}
             /> 
-          {/* <button 
-            className={btnClasses}
-            onClick={() => this.props.confirmButtonSubmit(this.props.id)}
-            disabled={btnOn}>
-              Confirmar
-          </button>
-          <button 
-            className="todos__list__item__edit__form__cancel x"
-            onClick={() => this.props.toogleEditTodo(this.props.id)}>
-              Cancelar
-          </button> */}
         </form>
       </li>
     );
